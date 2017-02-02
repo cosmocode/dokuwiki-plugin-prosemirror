2ef3eb028ee190b19f8609883b03a2811c8a82ef
Andreas Gohr <gohr@cosmocode.de>
2017-02-02 11:10:20 +0100

    have production and devel rules in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,25 @@
+/* globals process */
+
+// production rules
+const prod = {
+    "indent": ["error", 4],
+    "no-magic-numbers": ["warn", { "ignore": [0, 1]}],
+};
+
+// dev rules extend production rules
+const dev = Object.assign(
+    {
+        "no-console": 0,
+    },
+    prod
+);
+
+// decide which rules to use -- default to dev
+let rules = dev;
+if(process.env.ENV === 'prod') {
+    rules = prod;
+}
+
 module.exports = {
     "extends": "airbnb-base",
     "plugins": [
@@ -6,8 +28,5 @@ module.exports = {
     "env": {
         "browser": true,
     },
-    "rules": {
-        "indent": ["error", 4],
-        "no-magic-numbers": ["warn", { "ignore": [0, 1]}],
-    },
+    rules
 };
