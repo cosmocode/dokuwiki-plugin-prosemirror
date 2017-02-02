41eb51a63e0d1a1cd9dfe142141a1bc36f5a8159
Andreas Gohr <gohr@cosmocode.de>
2017-02-02 16:01:23 +0100

    allow id to be supplied to trial.php to load differnt pages

--- trial.php
+++ trial.php
@@ -13,8 +13,9 @@
 <textarea id="json" style="width: 100%; height: 300px"><?php
     if(!defined('DOKU_INC')) define('DOKU_INC', dirname(__FILE__) . '/../../../');
     require_once(DOKU_INC . 'inc/init.php');
-
-    $doc = p_cached_output(wikiFN('wiki:syntax'), 'prosemirror', 'wiki:syntax');
+    global $INPUT;
+    $id = $INPUT->str(id,'wiki:syntax');
+    $doc = p_cached_output(wikiFN($id), 'prosemirror', 'wiki:syntax');
     echo $doc;
     ?></textarea>
 
