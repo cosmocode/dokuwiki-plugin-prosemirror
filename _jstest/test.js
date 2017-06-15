af65506b65c856ff6d82a29069dd5f1eb4e1b734
Michael Große <grosse@cosmocode.de>
2017-06-15 12:10:56 +0200

    Use common source of testdata for phpunit and qunit

--- _jstest/test.js
+++ _jstest/test.js
@@ -1,17 +1,17 @@
 const { testJsonAgainstSchema } = require('./testmodule.js');
+const fs = require('fs');
 
 QUnit.module('testing json strings');
 
-const testData = [
-    { input: '{"type": "doc", "content": [{ "type": "paragraph", "content": [{"type": "text","text": "ABC DEF"}]}]}', msg: 'paragraph' },
-    { input: '{"type": "doc","content": [{"type": "text","text": " ABC DEF"}]}', msg: 'quotes' },
-];
+const path = __dirname;
+const buffer = fs.readFileSync(`${path}/../_test/testdata.json`);
+const testData = JSON.parse(buffer.toString());
 
 testData.forEach((data) => {
     QUnit.test(data.msg, (assert) => {
         let valid = true;
         try {
-            testJsonAgainstSchema(data.input);
+            testJsonAgainstSchema(data.json);
         } catch (e) {
             if (e instanceof RangeError) {
                 valid = false;
