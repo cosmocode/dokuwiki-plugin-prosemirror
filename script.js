85f0072eed7608788fad088788d62eb45867121d
Michael Große <grosse@cosmocode.de>
2018-06-01 14:19:47 +0200

    🔊 Log javascript errors to sentry and redirect to syntax editor

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 jQuery(() => {
 
-    jQuery('.plugin_prosemirror_useWYSIWYG').on('click', function() {
+    const $toggleEditorButton = jQuery('.plugin_prosemirror_useWYSIWYG');
+    $toggleEditorButton.on('click', function() {
         const $current = DokuCookie.getValue('plugin_prosemirror_useWYSIWYG');
         DokuCookie.setValue('plugin_prosemirror_useWYSIWYG', $current ? '' : '1');
     });
@@ -10,7 +11,30 @@ jQuery(() => {
         return;
     }
     $jsonField.attr('id', 'prosemirror_json');
-    /* DOKUWIKI:include lib/bundle.js */
+
+    try {
+        /* DOKUWIKI:include lib/bundle.js */
+    } catch (e) {
+        const $textArea = jQuery('#wiki__text');
+        let message = 'There was an error in the WYSIWYG editor. You will be redirected to the syntax editor in 5 seconds.';
+        if (window.logSentryException) {
+            logSentryException(e, {
+                tags: {
+                    plugin: 'prosemirror',
+                    'id': JSINFO.id,
+                },
+                extra: {
+                    'content': $textArea.val(),
+                }
+            });
+            message += ' The error has been logged to sentry.';
+        }
+        $textArea.replaceWith('<div class="error">'+ message + '</div>');
+        setTimeout(function() {
+            $toggleEditorButton.click();
+        }, 5000);
+        return;
+    }
 
     jQuery('#wiki__text').hide();
     jQuery('.editBox > .toolbar').hide();
