01748e22f6eb488a40b738a75c7db943cc045d2d
Michael Große <grosse@cosmocode.de>
2018-06-05 14:37:28 +0200

    ♻️ Use not enabled state as early return
    This makes it easier to add further conditions, i.e. whether the icon is not only enabled but also active

--- script/MenuView.js
+++ script/MenuView.js
@@ -19,8 +19,12 @@ class MenuView {
 
     update() {
         this.items.forEach(({ command, dom }) => {
-            const active = command(this.editorView.state, null, this.editorView);
-            dom.style.display = active ? '' : 'none'; // eslint-disable-line no-param-reassign
+            const enabled = command(this.editorView.state, null, this.editorView);
+            if (!enabled) {
+                dom.style.display = 'none'; // eslint-disable-line no-param-reassign
+                return;
+            }
+            dom.style.display = ''; // eslint-disable-line no-param-reassign
         });
     }
 
