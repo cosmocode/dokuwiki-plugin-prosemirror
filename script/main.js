7203493b4ebed88c2f3aaaf76904b0475264f3d1
Michael Große <grosse@cosmocode.de>
2018-06-14 15:24:32 +0200

    🔥 remove commented code

--- script/main.js
+++ script/main.js
@@ -10,61 +10,6 @@ import customKeymapPlugin from './keymap';
 import LinkView from './LinkView';
 import MediaView from './MediaView';
 
-
-//
-// const signatureMI = new MenuItem({
-//     select(state) {
-//         const validLocation = insertPoint(state.doc, state.selection.from, schema.nodes.text);
-//         return validLocation !== null;
-//     },
-//     run(state, dispatch) {
-//         const now = new Date().toLocaleString('en-GB'); // fixme replace with jqueryUI and dformt options: https://stackoverflow.com/a/24655570/3293343
-//         const username = 'Max Müller'; // fixme replace with username
-//         const usermail = 'mueller@example.com'; // fixme replace with usermail
-//         const prefix = ' --- ';
-//         const timestring = ` ${now}`;
-//         const usernameFrom = state.selection.$from.pos + prefix.length;
-//         const usernameTo = usernameFrom + username.length;
-//         const signatureEnd = usernameTo + timestring.length;
-//         dispatch(state.tr
-//             .ensureMarks([])
-//             .insertText(prefix)
-//             .insertText(username)
-//             .addMark(
-//                 usernameFrom,
-//                 usernameTo,
-//                 schema.marks.link.create({
-//                     href: `mailto:${usermail}`,
-//                 }),
-//             )
-//             .insertText(timestring)
-//             .addMark(
-//                 usernameFrom,
-//                 signatureEnd,
-//                 schema.marks.em.create(),
-//             )
-//             .ensureMarks([]));
-//         return true;
-//     },
-//     icon: {
-//         dom: (() => {
-//             const sigToolbarIcon = document.createElement('img');
-//             sigToolbarIcon.setAttribute('src', '../../images/toolbar/sig.png'); // fixme adjust path/consider svg
-//             return sigToolbarIcon;
-//         })(),
-//     },
-//     title: 'Insert Signature', // fixme use translated LANG string
-// });
-//
-// const content = buildMenuItems(schema).fullMenu;
-// content.push([signatureMI]);
-//
-//
-// const plugins = exampleSetup({
-//     schema,
-//     menuContent: content,
-// });
-
 // PLUGIN ORDER IS IMPORTANT!
 const plugins = [
     menu,
