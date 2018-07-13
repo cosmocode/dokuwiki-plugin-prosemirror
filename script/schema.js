453c8e3a054ba32b293db9f4a4e8a23156c15186
Michael Große <grosse@cosmocode.de>
2018-07-13 16:55:06 +0200

    ✨ support copying smileys from other DokuWiki pages
    This also "breaks" copying images, but that didn't work before either.
    This had to be done in order of preventing the image role to trigger for
    smileys as well. Fixing image parsing is its own ticket (#59).
    
    This further implements #48

--- script/schema.js
+++ script/schema.js
@@ -138,6 +138,16 @@ imageNode.attrs.cache = { default: '' };
 imageNode.attrs['data-resolvedHtml'] = { default: '' };
 imageNode.attrs.id = {};
 delete imageNode.attrs.src;
+imageNode.parseDOM = [{
+    tag: 'img.media[src]',
+    getAttrs: function getAttrs(dom) {
+        return {
+            src: dom.getAttribute('src'),
+            title: dom.getAttribute('title'),
+            alt: dom.getAttribute('alt'),
+        };
+    },
+}];
 nodes = nodes.update('image', imageNode);
 
 const imageAttrs = {};
@@ -186,6 +196,18 @@ nodes = nodes.addToEnd('smiley', {
         src: `${DOKU_BASE}/lib/images/smileys/${node.attrs.icon}`,
         alt: node.attrs.syntax,
         class: 'icon',
+    }],
+    parseDOM: [{
+        tag: 'img.icon',
+        getAttrs: (dom) => {
+            const src = dom.getAttribute('src').split('/');
+            const icon = src.pop();
+            if (!src.slice(-3).every((value, index) => ['lib', 'images', 'smileys'][index] === value)) {
+                return false;
+            }
+            const syntax = dom.getAttribute('alt');
+            return { icon, syntax };
+        },
     }],
 });
 
