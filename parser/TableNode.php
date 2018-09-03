133f42cc34049818c7a8501a977b5137bba0dafd
Michael Große <grosse@cosmocode.de>
2018-09-03 11:38:26 +0200

    🐛 (Tables) Fix broken rowspans in the last col(s) of a row

--- parser/TableNode.php
+++ parser/TableNode.php
@@ -8,12 +8,35 @@ class TableNode extends Node
     /** @var TableRowNode[] */
     protected $tableRows = [];
     protected $rowSpans = [];
+    protected $numCols;
 
     public function __construct($data)
     {
         foreach ($data['content'] as $row) {
             $this->tableRows[] = new TableRowNode($row, $this);
         }
+        $this->countColNum();
+    }
+
+    /**
+     * Count the total number of columns in the table
+     *
+     * This method calculates the number of columns in the first row, by adding the colspan of each cell.
+     * This produces the correct number columns, since the first row cannot have ommited cells due to a
+     * rowspan, as every other row could have.
+     *
+     */
+    protected function countColNum() {
+        $this->numCols = $this->tableRows[0]->countCols();
+    }
+
+    /**
+     * Get the total number of columns for this table
+     *
+     * @return int
+     */
+    public function getNumTableCols() {
+        return $this->numCols;
     }
 
     public function toSyntax()
