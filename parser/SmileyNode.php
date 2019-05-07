e0a50a13d46aa381f03a41b8dae655c0e985eb32
Anna Dabrowska <dabrowska@cosmocode.de>
2019-05-07 17:45:06 +0200

    Add missing marks handling to smiley node

--- parser/SmileyNode.php
+++ parser/SmileyNode.php
@@ -3,16 +3,22 @@
 namespace dokuwiki\plugin\prosemirror\parser;
 
 
-class SmileyNode extends Node
+class SmileyNode extends Node implements InlineNodeInterface
 {
 
     protected $parent;
     protected $data;
 
-    public function __construct($data, Node $parent)
+    /** @var TextNode */
+    protected $textNode;
+
+    public function __construct($data, Node $parent, Node $previous = null)
     {
         $this->parent = &$parent;
         $this->data = $data;
+
+        // every inline node needs a TextNode to track marks
+        $this->textNode = new TextNode(['marks' => $data['marks']], $parent, $previous);
     }
 
     /**
@@ -23,5 +29,23 @@ class SmileyNode extends Node
     public function toSyntax()
     {
         return $this->data['attrs']['syntax'];
+    }
+
+    /**
+     * @param string $markType
+     */
+    public function increaseMark($markType)
+    {
+        return $this->textNode->increaseMark($markType);
+    }
+
+    /**
+     * @param string $markType
+     * @return int|null
+     * @throws \Exception
+     */
+    public function getStartingNodeMarkScore($markType)
+    {
+        return $this->textNode->getStartingNodeMarkScore($markType);
     }
 }
