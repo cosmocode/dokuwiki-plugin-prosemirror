4f0a80def860ae6563dc9d09d1d9aafe30f699d7
AlexBlack788 <60142358+AlexBlack788@users.noreply.github.com>
2020-04-01 17:02:48 +0300

    Upgade CodeBlock
    New features: added two fields at the top of CodeBlock: start_line_numbers_at and highlight_lines_extra.
    
    Logic of the field start_line_numbers_at is as follows:
    1. The field accepts integer values - fractional values will be converted to integer values, non-numeric values - discarded.
    2. The value specified in the field controls the enable_line_numbers_at field as follows:
    2.1 The positive value sets enable_line_numbers="true".
    2.2 A negative value sets enable_line_numbers="false", the value of start_line_numbers_at will also be specified and become positive.
    2.3 An uninstalled (undefined) value sets enable_line_numbers="false".
    
    Logic of highlight_lines_extra field operation is as follows:
    1. The field accepts a single integer value or a set of integer values separated by a comma - fractional values will be converted to integers, not numeric values - discarded.
    2. The field is automatically recalculated when you switch to Prosemirror editor or preview from Prosemirror editor. 
    3. If a set of values is specified in the field, they will be sorted in ascending order.
    4.The possibility to specify "relative" values has been implemented - i.e. if, for example, we have a certain piece of code, the numbering of which starts with 37 lines, and we need to highlight the 53rd and 68th lines of code, then in the field we type: "53,68" (without quotes).  In the standard DokuWiki editor we will still have absolute values. 
    5. The ability to specify "absolute" values, as implemented in the standard DokuWiki editor is also saved - for this we specify negative values. I.e. to highlight, for example, 4 and 7 code stacks, no matter from which line our code begins to number - specify in the field: "-4,-7" (without quotes).
    6. If the field start_line_numbers_at is not specified, the numbering will be the same as in the standard DokuWiki editor, i.e. starting with 1.

--- parser/CodeBlockNode.php
+++ parser/CodeBlockNode.php
@@ -16,15 +16,77 @@ class CodeBlockNode extends Node
 
     public function toSyntax()
     {
+// 555
+		$TAG = $this->data['attrs'];
         $openingTag = '<code';
-        if (!empty($this->data['attrs']['data-language'])) {
-            $openingTag .= ' ' . $this->data['attrs']['data-language'];
+        if (!empty($TAG['data-language'])) {
+            $openingTag .= ' ' . $TAG['data-language'];
         } else {
             $openingTag .= ' -';
         }
-        if (!empty($this->data['attrs']['data-filename'])) {
-            $openingTag .= ' ' . $this->data['attrs']['data-filename'];
+        if (!empty($TAG['data-filename'])) {
+            $openingTag .= ' ' . $TAG['data-filename'];
         }
+		$extraTag = '';
+		if (isset($TAG['data-sln-old']))
+		{
+			$sln_old = $TAG['data-sln-old'];
+			if (is_numeric($sln_old))
+			{
+				$sln_old = (int) $sln_old;
+			}
+			else
+			{
+				$sln_old = 1;
+			}
+		}
+		if (isset($TAG['data-sln']))
+		{
+			$start_line_numbers_at = $TAG['data-sln'];
+			if (is_numeric($start_line_numbers_at))
+			{
+				$start_line_numbers_at = (int) $start_line_numbers_at;
+				if (!$extraTag) $extraTag = '[';
+				if ($start_line_numbers_at > 0) 
+					$extraTag .= 'enable_line_numbers="true", ';
+				else
+					$extraTag .= 'enable_line_numbers="false", ';
+				$extraTag .= 'start_line_numbers_at="' . abs($start_line_numbers_at) .'"';
+			}
+			else
+			{
+				$extraTag = '[enable_line_numbers="false"';
+			}
+		}
+		if (isset($TAG['data-hle']))
+		{
+			$highlight_lines_extra = $TAG['data-hle'];
+			$arr = explode(',', $highlight_lines_extra);
+			$str = '';
+			foreach($arr as $val)
+			{
+				if ($str) $str .= ',';
+				if (is_numeric($val)) 
+				{	
+					$ival = (int) $val;
+					if ($sln_old > 0 && $ival > 0)
+						$str .= $ival - $sln_old + 1;
+					else
+						$str .= abs($ival);
+				}
+			}
+			if ($str)
+			{
+				if (!$extraTag)
+					$extraTag = '[';
+				else 
+					$extraTag .= ', ';
+				$extraTag .= 'highlight_lines_extra="' . $str . '"]';
+			}
+			elseif ($extraTag) $extraTag .= ']';
+		}
+		if ($extraTag) $openingTag .= ' ' . $extraTag;
+// 555---
         $openingTag .= '>';
         return $openingTag . "\n" . $this->data['content'][0]['text'] . "\n</code>";
     }
