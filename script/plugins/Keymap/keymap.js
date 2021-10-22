c650ec292e38d6de13d95b965a8e291171308b94
solarkraft <1337470+solarkraft@users.noreply.github.com>
2021-10-23 00:35:34 +0200

    Bind save command to Mod-Enter

--- script/plugins/Keymap/keymap.js
+++ script/plugins/Keymap/keymap.js
@@ -2,6 +2,7 @@ import { keymap } from 'prosemirror-keymap';
 import { splitListItem } from 'prosemirror-schema-list';
 import { baseKeymap, chainCommands } from 'prosemirror-commands';
 import { redo, undo } from 'prosemirror-history';
+import { save } from '../../customCommands';
 
 function getKeymapPlugin(schema) {
     // Mac OS has its own typical shortcuts
@@ -10,6 +11,7 @@ function getKeymapPlugin(schema) {
     const customKeymap = {};
 
     customKeymap.Enter = splitListItem(schema.nodes.list_item); // eslint-disable-line import/no-named-as-default-member
+    customKeymap['Mod-Enter'] = save;
 
     const combinedKeymapUnion = Object.keys(customKeymap).reduce((acc, key) => {
         if (baseKeymap[key]) {
