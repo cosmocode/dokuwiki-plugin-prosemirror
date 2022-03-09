59d952032e6fea13b1fdd332d4a153f824544211
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-03-09 10:22:38 +0100

    French translation

---
+++ lang/fr/lang.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * French language file for prosemirror plugin
+ *
+ * @author Laurent Forthomme <lforthomme@protonmail.ch>
+ */
+
+// custom language strings for the plugin
+$lang['switch_editors'] = 'Activer/Désactiver l\'éditeur avancé';
+
+$lang['link target'] = 'Destination du lien';
+$lang['type:wiki page'] = 'Une page de ce wiki';
+$lang['type:interwiki'] = 'Un lien interwiki';
+$lang['type:email'] = 'Une adresse e-mail';
+$lang['type:external'] = 'Un lien vers une page externe';
+$lang['type:other'] = 'Autre';
+$lang['type:automatic title'] = 'Titre automatique';
+$lang['type:custom title'] = 'Titre personnalisé';
+$lang['type:image'] = 'Utiliser une image comme titre';
+
+$lang['placeholder:link name'] = '(automatique)';
+
+$lang['legend:media'] = 'Média ou fichier';
+$lang['label:image_properties'] = 'Propriétés de l\'image';
+$lang['legend:size'] = 'Dimensions';
+$lang['legend:alignment'] = 'Alignement';
+$lang['legend:linking'] = 'Lien';
+$lang['legend:caching'] = 'Cache';
+$lang['media target'] = 'Fichier image ou document';
+$lang['label:caption'] = 'Légende / texte du lien';
+$lang['label:width'] = 'Largeur (px)';
+$lang['label:height'] = 'Hauteur (px)';
+$lang['label:default alignment'] = 'Par défaut';
+$lang['label:float left'] = 'À gauche';
+$lang['label:center alignment'] = 'Centré';
+$lang['label:float right'] = 'À droite';
+$lang['label:default linking'] = 'Lien par défaut';
+$lang['label:direct linking'] = 'Lien direct';
+$lang['label:nolink'] = 'Pas de lien';
+$lang['label:linkonly'] = 'Lien uniquement';
+$lang['label:default caching'] = 'Défaut';
+$lang['label:recache'] = 'Re-caché';
+$lang['label:nocache'] = 'Non-caché';
+
+$lang['cancel'] = 'Annuler';
+$lang['js']['cancel'] = 'Annuler';
+
+$lang['js']['linkConfig'] = 'Configuration des liens';
+$lang['js']['mediaConfig'] = 'Configuration des médias';
+$lang['js']['rssConfig'] = 'Configuration des flux RSS';
+$lang['js']['footnoteViewTitle'] = 'Éditer la note de pied de page';
+$lang['js']['placeholder:page'] = 'namespace:page';
+
+$lang['js']['label:blockquote'] = 'Bloc de citation';
+$lang['js']['label:bulletList'] = 'Liste à points';
+$lang['js']['label:code'] = 'Bloc de code';
+$lang['js']['label:footnote'] = 'Ajouter une note de pied de page';
+$lang['js']['label:headings'] = 'Titres';
+$lang['js']['label:heading'] = 'Titre %s';
+$lang['js']['label:media'] = 'Insérer une image ou un fichier';
+$lang['js']['label:liftLI'] = 'Remonter le point d\'un niveau';
+$lang['js']['label:link'] = 'Insérer un lien';
+$lang['js']['label:orderedList'] = 'Liste ordonnée';
+$lang['js']['label:paragraph'] = 'Paragraphe';
+$lang['js']['label:pluginBlock'] = 'Bloc d\'extension';
+$lang['js']['label:rss'] = 'Ajouter un nouveau flux RSS';
+$lang['js']['label:smileys'] = 'Smileys';
+$lang['js']['label:settings'] = 'Paramètres de la page';
+$lang['js']['label:nocache'] = 'Désactiver le cache';
+$lang['js']['label:notoc'] = 'Cacher la table des matières';
+$lang['js']['label:table'] = 'Ajouter un nouveau tableau 2x2';
+$lang['js']['label:table-ops'] = 'Éditer le tableau';
+$lang['js']['label:table-add-row-before'] = 'Ajouter une ligne avant';
+$lang['js']['label:table-add-row-after'] = 'Ajouter une ligne après';
+$lang['js']['label:table-row-delete'] = 'Supprimer la ligne';
+$lang['js']['label:table-add-column-before'] = 'Ajouter une colonne avant';
+$lang['js']['label:table-add-column-after'] = 'Ajouter une colonne après';
+$lang['js']['label:table-column-delete'] = 'Supprimer une colonne';
+$lang['js']['label:table-cell-header-toggle'] = 'Activer/désactiver le titre';
+$lang['js']['label:table-cell-merge'] = 'Grouper les cellules';
+$lang['js']['label:table-cell-split'] = 'Diviser la cellule';
+$lang['js']['label:table-cell-align-left'] = 'Alignement à gauche';
+$lang['js']['label:table-cell-align-center'] = 'Alignement centré';
+$lang['js']['label:table-cell-align-right'] = 'Alignement à droite';
+$lang['js']['label:table-delete'] = 'Supprimer le tableau';
+$lang['js']['label:sinkListItem'] = 'Descendre le point d\'un niveau';
+$lang['js']['label:marks'] = 'Formatage';
+$lang['js']['label:strong'] = 'gras';
+$lang['js']['label:em'] = 'italique';
+$lang['js']['label:underline'] = 'souligné';
+$lang['js']['label:superscript'] = 'exposant';
+$lang['js']['label:subscript'] = 'indice';
+$lang['js']['label:deleted'] = 'rayé';
+$lang['js']['label:monospaced'] = 'monospacé';
+$lang['js']['label:undo'] = 'revenir en arrière';
+$lang['js']['label:redo'] = 'refaire';
+
+$lang['js']['label:rss url'] = 'Adresse du flux RSS';
+$lang['js']['label:rss nOfItems'] = 'Nombre maximum d\'éléments à afficher';
+$lang['js']['label:rss reverse'] = 'Afficher les derniers éléments du flux en premier';
+$lang['js']['label:rss author'] = 'Afficher les auteurs';
+$lang['js']['label:rss date'] = 'Afficher les dates de publication';
+$lang['js']['label:rss details'] = 'Afficher les détails';
+$lang['js']['label:rss refresh'] = 'Rafraîchir la période';
+
+$lang['js']['code_block_hint'] = '💡 Appuyez sur CTRL+Enter pour terminer';
