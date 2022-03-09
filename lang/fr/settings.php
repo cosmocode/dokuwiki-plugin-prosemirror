8d8d5fd170f8f9f99b64f879a88409f0086dfa03
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-03-09 10:30:35 +0100

    Also translated the settings string

---
+++ lang/fr/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * French language file for prosemirror plugin
+ *
+ * @author Laurent Forthomme <lforthomme@protonmail.ch>
+ */
+
+// keys need to match the config setting name
+$lang['forceWYSIWYG'] = 'Imposer l\'éditeur WYSIWYG à tous les utilisateur, à l\'exception des gestionnaires et administrateurs';
