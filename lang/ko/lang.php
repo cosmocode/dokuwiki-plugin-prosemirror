b1393e6eeb320b3b746a17ddd0c76aab170fffba
Juyoung Lee <ejuyoung@kaist.ac.kr>
2024-02-05 13:20:22 +0900

    Add Korean Language

---
+++ lang/ko/lang.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Korean language file for prosemirror plugin
+ *
+ * @author sebaram <ejuyoung@gmail.com>
+ */
+
+// custom language strings for the plugin
+$lang['switch_editors'] = '전환: 위키문법 &#11020; 비주얼 편집기';
+
+$lang['link target'] = '링크 대상';
+$lang['type:wiki page'] = '본 위키 문서';
+$lang['type:interwiki'] = '인티위키 링크';
+$lang['type:email'] = '이메일 주소';
+$lang['type:external'] = '외부페이지 링크';
+$lang['type:other'] = '기타';
+$lang['type:automatic title'] = '제목 자동 생성';
+$lang['type:custom title'] = '제목 직접 입력';
+$lang['type:image'] = '제목으로 이미지 사용';
+
+$lang['placeholder:link name'] = '(자동 생성)';
+
+$lang['legend:media'] = '미디어 또는 파일';
+$lang['label:image_properties'] = '이미지 속성';
+$lang['legend:size'] = '크기';
+$lang['legend:alignment'] = '정렬';
+$lang['legend:linking'] = '링킹';
+$lang['legend:caching'] = '캐싱';
+$lang['media target'] = '이미지 또는 문서 파일';
+$lang['label:caption'] = '캡션 / 링크 텍스트';
+$lang['label:width'] = '너비 (px)';
+$lang['label:height'] = '높이 (px)';
+$lang['label:default alignment'] = '기본값';
+$lang['label:float left'] = '왼쪽';
+$lang['label:center alignment'] = '가운데';
+$lang['label:float right'] = '오른쪽';
+$lang['label:default linking'] = '기본값';
+$lang['label:direct linking'] = '직접 링크';
+$lang['label:nolink'] = '링크 없음';
+$lang['label:linkonly'] = '링크만';
+$lang['label:default caching'] = '기본값';
+$lang['label:recache'] = '재캐싱';
+$lang['label:nocache'] = '캐싱하지 않음';
+
+$lang['cancel'] = '취소';
+$lang['js']['cancel'] = '취소';
+
+$lang['js']['linkConfig'] = '링크 설정';
+$lang['js']['mediaConfig'] = '미디어 설정';
+$lang['js']['rssConfig'] = 'RSS-피드 설정';
+$lang['js']['footnoteViewTitle'] = '각주(footnote) 편집';
+$lang['js']['placeholder:page'] = 'namespace:page';
+
+$lang['js']['label:blockquote'] = '인용구(Blockquote)';
+$lang['js']['label:bulletList'] = '글머리 기호 목록(bullet list)으로 서식 지정';
+$lang['js']['label:code'] = '코드 블록(Code Block)';
+$lang['js']['label:footnote'] = '각주(footnote) 추가';
+$lang['js']['label:headings'] = '제목(Headings)';
+$lang['js']['label:heading'] = '제목 %s단계';
+$lang['js']['label:media'] = '이미지 또는 파일 삽입';
+$lang['js']['label:liftLI'] = '목록 항목 올리기';
+$lang['js']['label:link'] = '링크 삽입';
+$lang['js']['label:orderedList'] = '순서있는 목록(ordered list)으로 서식 지정';
+$lang['js']['label:paragraph'] = '문단';
+$lang['js']['label:pluginBlock'] = '플러그인 블록';
+$lang['js']['label:rss'] = '새 RSS 피드 추가';
+$lang['js']['label:smileys'] = '스마일리';
+$lang['js']['label:settings'] = '페이지 설정';
+$lang['js']['label:nocache'] = '캐시 비활성화';
+$lang['js']['label:notoc'] = '목차 숨기기';
+$lang['js']['label:table'] = '새 2x2 테이블 추가';
+$lang['js']['label:table-ops'] = '테이블 편집';
+$lang['js']['label:table-add-row-before'] = '앞에 행 추가';
+$lang['js']['label:table-add-row-after'] = '뒤에 행 추가';
+$lang['js']['label:table-row-delete'] = '행 삭제';
+$lang['js']['label:table-add-column-before'] = '앞에 열 추가';
+$lang['js']['label:table-add-column-after'] = '뒤에 열 추가';
+$lang['js']['label:table-column-delete'] = '열 삭제';
+$lang['js']['label:table-cell-header-toggle'] = '헤더 상태 전환';
+$lang['js']['label:table-cell-merge'] = '셀 병합';
+$lang['js']['label:table-cell-split'] = '셀 분할';
+$lang['js']['label:table-cell-align-left'] = '왼쪽 정렬';
+$lang['js']['label:table-cell-align-center'] = '가운데 정렬';
+$lang['js']['label:table-cell-align-right'] = '오른쪽 정렬';
+$lang['js']['label:table-delete'] = '테이블 삭제';
+$lang['js']['label:sinkListItem'] = '목록 항목 내리기';
+$lang['js']['label:marks'] = '글꼴 서식';
+$lang['js']['label:strong'] = '굵게';
+$lang['js']['label:em'] = '이탤릭체';
+$lang['js']['label:underline'] = '밑줄';
+$lang['js']['label:superscript'] = '위 첨자';
+$lang['js']['label:subscript'] = '아래 첨자';
+$lang['js']['label:deleted'] = '취소선';
+$lang['js']['label:monospaced'] = '고정폭';
+$lang['js']['label:unformatted'] = '위키 서식 무시';
+$lang['js']['label:undo'] = '실행 취소';
+$lang['js']['label:redo'] = '다시 실행';
+
+$lang['js']['label:rss url'] = 'RSS 피드의 URL';
+$lang['js']['label:rss nOfItems'] = '표시할 최대 항목 수';
+$lang['js']['label:rss reverse'] = '피드의 마지막 항목을 먼저 표시';
+$lang['js']['label:rss author'] = '항목 작성자 이름 표시';
+$lang['js']['label:rss date'] = '항목 날짜 표시';
+$lang['js']['label:rss details'] = '항목 설명 표시';
+$lang['js']['label:rss refresh'] = '새로고침 주기';
+
+$lang['js']['code_block_hint'] = '💡 CTRL+Enter를 눌러 종료하세요';
---
+++ lang/ko/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Korean language file for prosemirror plugin
+ *
+ * @author sebaram <ejuyoung@gmail.com>
+ */
+
+// keys need to match the config setting name
+$lang['forceWYSIWYG'] = '매니저/관리자를 제외한 모든 유저에게 WYSIWYG편집기 강제하기';
