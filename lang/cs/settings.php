5c07ddd71a8ae590b58d54ac9058788172468852
Martin Růžička <martinr@post.cz>
2024-07-24 04:10:45 +0200

    translation update

---
+++ lang/cs/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Martin Růžička <martinr@post.cz>
+ */
+$lang['forceWYSIWYG']          = 'Vynutit WYSIWYG editor pro všechny uživatele kromě manažerů a správců';
